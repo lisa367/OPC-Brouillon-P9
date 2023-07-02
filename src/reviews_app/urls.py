0172eb5cd1d5fa0770052630b02c6556f9fac7d1
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-07-03 01:44:33 +0200

    modified reviews urls links

--- src/reviews_app/urls.py
+++ src/reviews_app/urls.py
@@ -4,7 +4,7 @@ from .views import CreateReviewView, UpdateReviewView, DeleteReviewView
 app_name = "reviews_app"
 
 urlpatterns = [
-    path("create/", CreateReviewView.as_view(), name="create-critique"),
-    path("update/<review_id>", UpdateReviewView.as_view(), name="update-critique"),
-    path("delete/<review_id>", DeleteReviewView.as_view(), name="delete-critique"),
+    path("create/", CreateReviewView.as_view(), name="create-review"),
+    path("update/<review_id>", UpdateReviewView.as_view(), name="update-review"),
+    path("delete/<review_id>", DeleteReviewView.as_view(), name="delete-review"),
 ]
