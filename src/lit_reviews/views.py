dfdbdffb8dbb10ccc3d3aa1e02d45b26bc48ff0e
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-07-04 17:36:05 +0200

    lit-reviews views - subscriptions function

--- src/lit_reviews/views.py
+++ src/lit_reviews/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+
 # from .models import UserFollows, Review, Ticket
 from .forms import SignupForm, LoginForm, NewSubscriptionForm
 from reviews_app.models import Review, UserFollows
@@ -58,19 +59,34 @@ def homepage(request):
 
 @login_required
 def subscriptions(request):
-    form = NewSubscriptionForm()
-    form = NewSubscriptionForm(request.POST)
-    if form.is_valid():
+    context = {}
+    if request.method == "POST":
+        # form = NewSubscriptionForm()
         form = NewSubscriptionForm(request.POST)
+        if form.is_valid():
+            form = NewSubscriptionForm(request.POST)
+    else:
+        form = NewSubscriptionForm()
+        user = User.objects.get(username=request.user.username)
+        followers_ids = UserFollows.objects.filter(followed_user=user.pk)
+        followers = [User.objects.get(pk=user_id).username for user_id in followers_ids]
+        followed_users = UserFollows.objects.filter(user=user.pk)
+
+        context["form"] = form
+        context["followed_users"] = followed_users
+        context["followers"] = followers
+        return render(request, "lit_reviews/user_follows.html", context=context)
 
 
 def unfollow(request, subs_username):
     user_name = request.user.username
     user = User.objects.get(username=user_name)
     followed_user = User.objects.get(username=subs_username)
-    user_subcription = UserFollows.objects.filter(user=user.pk, followed_user=followed_user.pk)
+    user_subcription = UserFollows.objects.filter(
+        user=user.pk, followed_user=followed_user.pk
+    )
     user_subcription.delete()
-    return redirect('homepage')
+    return redirect("homepage")
 
 
 @login_required
@@ -78,10 +94,9 @@ def posts(request):
     context = {"reviews": reviews, "tickets": tickets}
     reviews = Review.objects.all().order_by("-time_created")[0:5]
     tickets = Ticket.objects.all().order_by("-time_created")[0:5]
-    return render(request, 'lit_reviews/posts.html', context=context)
+    return render(request, "lit_reviews/posts.html", context=context)
 
-
-
+""" 
 @login_required
 def login_view(request):
     username = request.POST["username"]
@@ -89,12 +104,12 @@ def login_view(request):
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
+        redirect("homepage")
         # Redirect to a success page.
-        pass
     else:
         # Return an 'invalid login' error message.
         pass
-
+"""
 
 def logout_view(request):
     logout(request)
