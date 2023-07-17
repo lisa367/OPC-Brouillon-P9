b400b73b7d58ceca5749a0ba84565daf3e9b4f66
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-07-17 16:55:27 +0200

    added the css static file

--- src/lit_reviews/settings.py
+++ src/lit_reviews/settings.py
@@ -71,6 +71,10 @@ TEMPLATES = [
     },
 ]
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 WSGI_APPLICATION = "lit_reviews.wsgi.application"
 
 
@@ -121,9 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# STATIC_ROOT = BASE_DIR / "static"
 STATIC_URL = "static/"
 
-MEDIA_ROOT = BASE_DIR / "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
 
 # Default primary key field type
