021c731367e31a9f5055657185e645fe384eec50
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-07-20 23:59:18 +0200

    class views

--- src/reviews_app/views.py
+++ src/reviews_app/views.py
@@ -1,27 +1,32 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Review
 from .forms import ReviewForm
 
+
 class CreateReviewView(CreateView):
-	template_name = "critiques_app/create_review.html"
-	model = Review
-	form_class = ReviewForm
+    template_name = "reviews_app/create_review.html"
+    model = Review
+    form_class = ReviewForm
+    success_url = reverse_lazy("homepage")
     # fields = "__all__"
-		# exclude = ["time_created"]
-		# form_class = ReviewForm
+    # exclude = ["time_created"]
+    # form_class = ReviewForm
+
 
 class UpdateReviewView(UpdateView):
-	template_name = "critiques_app/update_review.html"
-	model = Review
-	form_class = ReviewForm
+    template_name = "reviews_app/update_review.html"
+    model = Review
+    form_class = ReviewForm
+    success_url = reverse_lazy("homepage")
     # fields = "__all__"
-		# exclude = ["time_created"]
+    # exclude = ["time_created"]
+
 
 class DeleteReviewView(DeleteView):
-	template_name = "critiques_app/delete_review.html"
-	model = Review
-	success_url = reverse_lazy("homepage")
+    template_name = "reviews_app/delete_review.html"
+    model = Review
+    success_url = reverse_lazy("homepage")
     # fields = "__all__
