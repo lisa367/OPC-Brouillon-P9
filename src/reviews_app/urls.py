355fb274c93a848230cd16917d5f08bd035cf48d
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-07-26 00:01:10 +0200

    reviews_app urls - create-review url with ticket id argument

--- src/reviews_app/urls.py
+++ src/reviews_app/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from .views import CreateReviewView, UpdateReviewView, DeleteReviewView, create_review_and_ticket
+from .views import (
+    CreateReviewView,
+    UpdateReviewView,
+    DeleteReviewView,
+    create_review_and_ticket,
+)
 
 app_name = "reviews_app"
 
 urlpatterns = [
-    path("create/", CreateReviewView.as_view(), name="create-review"),
+    path("create/<int:ticket_id>", CreateReviewView.as_view(), name="create-review"),
     path("update/<int:pk>", UpdateReviewView.as_view(), name="update-review"),
     path("delete/<int:pk>", DeleteReviewView.as_view(), name="delete-review"),
-
     path("create-review-and-ticket/", create_review_and_ticket, name="create-review-ticket"),
 ]
