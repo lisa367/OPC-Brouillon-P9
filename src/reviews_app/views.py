8851b39896026001b65b3638d6198c9a7bcb37a4
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-07-26 19:14:09 +0200

    reviews_app views - create_review_and_ticket function

--- src/reviews_app/views.py
+++ src/reviews_app/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Review
 from .forms import ReviewForm
 from tickets_app.forms import TicketForm
+from tickets_app.models import Ticket
 
 
 class CreateReviewView(CreateView):
@@ -15,6 +16,11 @@ class CreateReviewView(CreateView):
     # fields = "__all__"
     # exclude = ["time_created"]
     # form_class = ReviewForm
+
+    def get_context_data(self, ticket_id, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["ticket"] = Ticket.objects.get(ticket=ticket_id)
+        return context
 
 
 class UpdateReviewView(UpdateView):
@@ -33,20 +39,21 @@ class DeleteReviewView(DeleteView):
     # fields = "__all__
 
 
-
 def create_review_and_ticket(request):
     context = {}
     if request.method == "POST":
-        pass
         form_ticket = TicketForm(request.POST)
         form_review = ReviewForm(request.POST)
-        form_ticket.save()
-        form_review.save()
+        print(form_ticket.is_valid(), form_review.is_valid())
+        if all([form_ticket.is_valid(), form_review.is_valid()]):
+            form_ticket.save(commit=False)
+            print("ticket : ", form_ticket.cleaned_data)
+            print("review : ", form_review.cleaned_data)
+        return redirect("homepage")
     else:
         form_ticket = TicketForm()
         form_review = ReviewForm()
-    
-    context["form_ticket"] = form_ticket
-    context["form_review"] = form_review
+        context["form_ticket"] = form_ticket
+        context["form_review"] = form_review
 
-    return render("reviews_app/create_review_ticket.html", context=context)
+    return render(request, "reviews_app/create_review_ticket.html", context=context)
